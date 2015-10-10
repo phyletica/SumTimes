7f0986ccef608222b3ca81af9b38bff077cb98a1
Jamie Oaks <joaks1@gmail.com>
2015-10-09 17:04:16 -0700

    Working on multiprocessing infrastructure.

--- sumdivtimes.py
+++ sumdivtimes.py
@@ -9,13 +9,20 @@ import sys
 import os
 import re
 import io
+import collections
 import gzip
 import tempfile
 import argparse
 import logging
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+import multiprocessing
 
 logging.basicConfig(level=logging.INFO)
 _LOG = logging.getLogger(os.path.basename(__file__))
+_LOCK = multiprocessing.Lock()
 
 _program_info = {
     'name': os.path.basename(__file__),
@@ -105,7 +112,7 @@ def is_gzipped(file_path):
     """
 
     try:
-        fs = gzip.open(expand_path(file_path))
+        fs = gzip.open(file_path)
         d = next(fs)
         fs.close()
     except:
@@ -147,7 +154,7 @@ class ReadFile(object):
     open_files = set()
 
     def __init__(self, path):
-        self.path = expand_path(path)
+        self.path = path
         self.gzipped = is_gzipped(self.path)
         self.encoding = 'utf-8'
         if self.gzipped:
@@ -278,6 +285,13 @@ class PosteriorSampleDataError(SumDivTimesError):
         SumDivTimesError.__init__(self, *args, **kwargs)
 
 
+class PosteriorManager(object):
+    """
+    Contains all PosteriorSamples, runs their workers and gets results
+    back where they need to go
+    """
+    pass
+
 
 class PosteriorSample(object):
     """
@@ -289,6 +303,8 @@ class PosteriorSample(object):
     following key-value pairs:
 
     -   paths : A list of strings that specify paths to tree files.
+    -   schema : A string specifying the format of the tree files (default:
+        'nexus').
     -   tip_subsets : A list of sets of key-value pairs that will each initiate
         a ``TipSubset`` instance.
 
@@ -296,8 +312,6 @@ class PosteriorSample(object):
 
     -   name : A string.
     -   burnin : An integer.
-    -   schema : A string specifying the format of the tree files (default:
-        'nexus').
 
     An example of initiating an instance:
     >>> d = {'paths': ['test-data/trees/test.trees.gz'],
@@ -307,7 +321,7 @@ class PosteriorSample(object):
     >>> ps = PosteriorSample(**d)
     >>> len(ps.paths) == 1
     True
-    >>> ps.paths == (expand_path(d['paths'][0]),)
+    >>> ps.paths == (d['paths'][0],)
     True
     >>> len(ps.tip_subsets) == 1
     True
@@ -316,7 +330,6 @@ class PosteriorSample(object):
     """
 
     count = 0
-
     def __init__(self, *args, **kwargs):
         self.__class__.count += 1
         self.name = kwargs.pop('name',
@@ -325,27 +338,232 @@ class PosteriorSample(object):
         if not paths:
             raise PosteriorSampleDataError("A posterior sample must contain a "
                     "list of 'paths'")
-        self.paths = tuple(expand_path(p) for p in paths)
+        self.paths = tuple(p for p in paths)
+        self.schema = kwargs.pop('schema', None)
+        if not schema:
+            raise PosteriorSampleDataError("A posterior sample must specify "
+                    "the 'schema' of the tree files")
         tip_subsets = kwargs.pop('tip_subsets', None)
         if not tip_subsets:
             raise PosteriorSampleDataError("A posterior sample must contain a "
                     "list of 'tip_subsets'")
-        self.tip_subsets = [TipSubset(**d) for d in tip_subsets]
+        tip_subset_list = [TipSubset(**d) for d in tip_subsets]
+        self.tip_subset_map = collections.OrderedDict(zip(
+                [id(ts) for ts in tip_subset_list],
+                tip_subset_list))
         self.burnin = int(kwargs.pop('burnin', 0))
-        self.schema = kwargs.pop('schema', 'nexus')
         if len(kwargs) > 0:
             _LOG.warning("Unexpected attributes in posterior sample {0!r}: "
                     "{1}".format(self.name, ", ".join(kwargs.keys())))
-        self.processed = False
 
-    def process_trees(self):
-        if self.processed:
-            return
-        for p in self.paths:
-            with OpenFile(p) as tree_stream:
-        # taxon_namespace = dendropy.TaxonNamespace()
-        # tree_list = dendropy.TreeList()
-        # tree_list.read(
+    def _get_tip_subsets(self):
+        return self.tip_subset_map.values()
+
+    tip_subsets = property(_get_tip_subsets)
+
+
+class PosteriorWorker(object):
+    """
+    These instances are assigned a single posterior tree file from which to
+    extract divergence times
+    """
+
+    count = 0
+    def __init__(self, path, schema, burnin, tip_subsets, label = None):
+        self.__class__.count += 1
+        self.name = self.__class__.__name__ + '-' + str(self.count)
+        self.path = path
+        self.schema = schema
+        self.burnin = burnin
+        self.tip_subsets = tip_subsets
+        self.label = label
+        self.finished = False
+        self.div_times = dict(zip(
+                [id(ts) for ts in self.tip_subsets],
+                [[] for i in range(len(self.tip_subsets))]))
+        self.error = None
+        self.trace_back = None
+
+    def extract_div_times(self):
+        try:
+            return self._extract_div_times()
+        except Exception as e:
+            self.error = e
+            f = StringIO()
+            traceback.print_exc(file=f)
+            self.trace_back = f.getvalue()
+
+
+    def _extract_div_times(self):
+        with ReadFile(self.path) as tree_stream:
+            kwargs = {}
+            if self.schema.lower() == 'nexus':
+                kwargs['preserve_underscores'] = True
+            tree_iter = dendropy.Tree.yield_from_files(
+                    files = [tree_stream],
+                    schema = self.schema,
+                    tree_offset = self.burnin,
+                    preserve_underscores = True)
+            for tree_idx, tree in enumerate(tree_iter):
+                if not tree.is_rooted:
+                    raise Exception('Tree {0} in {1!r} is not rooted'.format(
+                            i + 1,
+                            self.path))
+                for tip_subset in self.tip_subsets:
+                    mrca_node = tree.mrca(taxon_labels = tip_subset.tips)
+                    target_node = mrca_node
+                    if target_node.parent_node and tip_subset.stem_based:
+                        target_node = mrca_node.parent_node
+                    self.div_times[id(tip_subset)].append((
+                            id(target_node),
+                            target_node.age))
+
+class JobManager(multiprocessing.Process):
+    count = 0
+    def __init__(self,
+            work_queue = None,
+            result_queue = None,
+            get_timeout = 0.4,
+            put_timeout = 0.2,
+            log = None,
+            lock = None):
+        multiprocessing.Process.__init__(self)
+        self.__class__.count += 1
+        self.name = self.__class__.__name__ + '-' + str(self.count)
+        if not work_queue:
+            work_queue = multiprocessing.Queue()
+        if not result_queue:
+            result_queue = multiprocessing.Queue()
+        self.work_queue = work_queue
+        self.result_queue = result_queue
+        self.get_timeout = get_timeout
+        self.put_timeout = put_timeout
+        if not log:
+            log = _LOG
+        self.log = log
+        if not lock:
+            lock = _LOCK
+        self.lock = lock
+        self.killed = False
+
+    def compose_msg(self, msg):
+        return '{0} ({1}): {2}'.format(self.name, self.pid, msg)
+
+    def send_msg(self, msg, method_str='info'):
+        self.lock.acquire()
+        try:
+            getattr(self.log, method_str)(self.compose_msg(msg))
+        finally:
+            self.lock.release()
+
+    def send_debug(self, msg):
+        self.send_msg(msg, method_str='debug')
+
+    def send_info(self, msg):
+        self.send_msg(msg, method_str='info')
+
+    def send_warning(self, msg):
+        self.send_msg(msg, method_str='warning')
+
+    def send_error(self, msg):
+        self.send_msg(msg, method_str='error')
+
+    def _get_worker(self):
+        worker = None
+        try:
+            self.send_debug('getting worker')
+            worker = self.work_queue.get(block=True, timeout=self.get_timeout)
+            self.send_debug('received worker {0}'.format(
+                    getattr(worker, 'name', 'nameless')))
+            # without blocking processes were stopping when the queue
+            # was not empty, and without timeout, the processes would
+            # hang waiting for jobs.
+        except queue.Empty:
+            time.sleep(0.2)
+            if not self.work_queue.empty():
+                self.send_warning('raised queue.Empty, but queue is '
+                        'not empty... trying again')
+                return self._get_worker()
+            else:
+                self.send_info('work queue is empty')
+        return worker
+
+    def _put_worker(self, worker):
+        try:
+            self.send_debug('returning worker {0}'.format(
+                    getattr(worker, 'name', 'nameless')))
+            self.result_queue.put(worker, block=True, timeout=self.put_timeout)
+            self.send_debug('worker {0} returned'.format(
+                    getattr(worker, 'name', 'nameless')))
+        except queue.Full, e:
+            time.sleep(0.2)
+            if not self.result_queue.full():
+                self.send_warning('raised queue.Full, but queue is '
+                        'not full... trying again')
+                self._put_worker(worker)
+            else:
+                self.send_error('result queue is full... aborting')
+            self.killed = True
+            raise e
+
+    def run(self):
+        self.send_debug('starting run')
+        while not self.killed:
+            worker = self._get_worker()
+            if worker is None:
+                break
+            self.send_info('starting worker {0}'.format(
+                    getattr(worker, 'name', 'nameless')))
+            worker.extract_div_times()
+            self.send_info('worker {0} finished'.format(
+                    getattr(worker, 'name', 'nameless')))
+            self._put_worker(worker)
+        if self.killed:
+            self.send_error('job manager was killed!')
+        self.send_debug('end run')
+
+
+    @classmethod
+    def run_workers(cls,
+            workers,
+            num_processors,
+            get_timeout = 0.4,
+            put_timeout = 0.2,
+            queue_max = 500):
+        work_queue = multiprocessing.Queue()
+        result_queue = multiprocessing.Queue()
+        finished = []
+        for w_list in list_splitter(workers, queue_max, by_size = True):
+            assert work_queue.empty()
+            assert result_queue.empty()
+            for w in w_list:
+                work_queue.put(w)
+            managers = []
+            for i in range(num_processors):
+                m = cls(work_queue = work_queue,
+                        result_queue = result_queue,
+                        get_timeout = get_timeout,
+                        put_timeout = put_timeout)
+                managers.append(m)
+            for i in range(len(managers)):
+                managers[i].start()
+            for i in range(len(w_list)):
+                _LOG.debug('JobManager.run_workers: getting result...')
+                w_list[i] = result_queue.get()
+                _LOG.debug('JobManager.run_workers: got result {0}'.format(
+                        getattr(w_list[i], 'name', 'nameless')))
+            for i in range(len(managers)):
+                managers[i].join()
+            for w in w_list:
+                if getattr(w, "error", None):
+                    _LOG.error('Worker {0} returned with an error:\n{1}'.format(
+                            getattr(w, 'name', 'nameless'),
+                            w.trace_back))
+                    raise w.error
+            assert work_queue.empty()
+            assert result_queue.empty()
+            finished.extend(w_list)
+        return finished
 
 
 class TipSubset(object):
